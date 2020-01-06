c4601f2deb341ebd053785940651905c7a0ecd8d
yogeswar <yogeswar@FULL084-AIR.FULL.Creative>
2020-01-06 10:44:01 +0530

    react crash course. Todolist

---
+++ src/components/TodoItem.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+export class TodoItem extends Component {
+
+    getStyle = () => {
+        return {
+            background : '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        }
+    }
+    render() {
+        const {id, title} = this.props.todo;
+        return (
+            <div style = {this.getStyle()}>
+                <p>
+                    <input type ="checkbox" onChange ={this.props.markComplete.bind(this,id)}/> {' '}
+                    { title }
+                <button onClick ={ this.props.delTodo.bind(this,id) } style ={btnStyle}>x</button>
+                </p>
+            </div>
+        )
+    }
+}
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired,
+    markComplete: PropTypes.func.isRequired,
+      delTodo: PropTypes.func.isRequired
+}
+const btnStyle ={
+    background: '#ff0000',
+    borderRadius: '50%',
+    color: '#fff',
+    border:'none',
+    outline: 'none',
+    padding: '5px 10px',
+    cursor: 'pointer',
+    float:'right'
+}
+
+export default TodoItem
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1> About</h1>
+            <p> This is my first react project I followed it up mostly</p>
+        </React.Fragment>
+    )
+}
+
+export default About
